ac0cfc8d5541d40e9beee5abf452e066c503682c
mairisaar <mairi.saar@khk.ee>
2018-10-10 16:26:26 +0300

    Inkrement ja sellega seotud funktsionaalsus.

--- main.js
+++ main.js
@@ -1,28 +1,47 @@
 //DOM Elements
 let numInput = document.getElementById('numInput');
-let factOutput = document.querySelector('#factOutput')
+let factOutput = document.querySelector('#factOutput');
+let incInput = document.querySelector('#incInput');
 
 //fetch API
 const getFact = () => {
+    factOutput.innerHTML = '';
+    
     let number = numInput.value;
+    let inc = incInput.value;
+    
+    let baseUrl = 'http://numbersapi.com/';
+    
+    let url = `${baseUrl}${number}..${parseInt(number) + parseInt(inc)}`; 
     
     /*if (number != ''){
         fethc('http://numbersapi.com/'+number)
         .then(response => response.text())
         .then(data => {
             console.log(data);
+            factOutput.innerText = response.data;
         })
         .catch(error => {
             console.log(error);
         })
     } */
     
-    if (number != '')
+    
+    
+    if (number != '' && inc != '')
         
-        axios.get('http://numbersapi.com/'+number)
+        axios.get(url)
         .then(response => {
-            console.log(response.data);
-            factOutput.innerText = response.data;
+            //console.log(response.data); 
+            //factOutput.innerText = response.data;
+            for (let item in response.data) {
+                console.log(item);
+                let li = document.createElement('li');
+                li.innerText = response.data[item];
+                factOutput.appendChild(li)
+            }
+                
+            
         })
         .catch(error => {
             console.log(error);
@@ -32,3 +51,4 @@ const getFact = () => {
 
 //Listening to events
 numInput.addEventListener('input', getFact);
+incInput.addEventListener('input', getFact);
