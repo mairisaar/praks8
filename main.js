367fdb6a9f76e0f33bffab3d89698196436b2bec
mairisaar <mairi.saar@khk.ee>
2018-10-10 14:30:06 +0300

    Esimene commit.

---
+++ main.js
@@ -0,0 +1,9 @@
+//DOM Elements
+let numInput = document.getElementById('numInput');
+
+//Listening to events
+numInput.addEventListener('input', e => {
+    let num = e.target.value;
+    
+    result = console.log('ter');
+});
