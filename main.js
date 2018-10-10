7c0ccf8d1cf4f3958ba1a2e15c0e5934df0fea22
mairisaar <mairi.saar@khk.ee>
2018-10-10 15:26:05 +0300

    Lisatud fakti näitamise funktsionaalsus.

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 //DOM Elements
 let numInput = document.getElementById('numInput');
+let factOutput = document.querySelector('#factOutput')
+
+//fetch API
+const getFact = () => {
+    let number = numInput.value;
+    
+    /*if (number != ''){
+        fethc('http://numbersapi.com/'+number)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    } */
+    
+    if (number != '')
+        
+        axios.get('http://numbersapi.com/'+number)
+        .then(response => {
+            console.log(response.data);
+            factOutput.innerText = response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
 
 //Listening to events
-numInput.addEventListener('input', e => {
-    let num = e.target.value;
-    
-    result = console.log('ter');
-});
+numInput.addEventListener('input', getFact);
